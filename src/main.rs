852eb429f4a5773c4f4a5c423fc31e7d206f1164
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-09 05:56:33 +0530

    added a loop and break statement

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,26 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("the secret number is {secret_number}");
 
-    println!("please input your guess:");
-    let mut guess = String::new();
+    loop {    
+        println!("please input your guess:");
+        let mut guess = String::new();
     
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
 
-    println!("you guessed: {guess}");
+        println!("you guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too large"),
-        Ordering::Equal => println!("YOu win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal => {
+                println!("YOu win");
+                break;
+                },
+            }
+        }
     }
-}
